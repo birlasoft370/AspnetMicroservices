74a82c9cf34fce01ee1da07d0db163e8f5bd5d60
birlasoft370 <balaintvl@gmail.com>
2023-02-28 12:46:49 +0530

    Discount API

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Discount.API.Entities
+{
+    [Table("Coupon",Schema ="dbo")]
+    public class Coupon
+    {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("ProductName")]
+        public string ProductName { get; set; }
+        [Column("Description")]
+        public string Description { get; set; }
+        [Column("Amount")]
+        public int Amount { get; set; }
+    }
+}
